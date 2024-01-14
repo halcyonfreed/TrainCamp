5eddf373383b50c95ff69a2eae60f1ae59cb2132
halcyonfreed <2846011241@qq.com>
2024-01-14 10:51:50 +0800

    day5

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,27 +1,25 @@
-#include<iostream>
-#include<vector>
-using namespace std;
+struct ListNode{
+    int val;
+    ListNode* next;
+    ListNode(int val): val(val),next(nullptr){} //不用分号，不然是空析构,;空的
+    ListNode():val(0),next(nullptr){}
+    ListNode(int val, ListNode* next):val(val),next(next){}
+};
 class Solution{
 public:
-    int removeElement(vector<int>& nums, int target){
-        int slow=0;
-        for(int fast=0; fast < nums.size(); ++fast){
-            if(target != nums[fast]) nums[slow++]=nums[fast];
-        }
-        return slow;
+    ListNode* removeElements(ListNode* head, int val){
+        ListNode* dummyhead=new ListNode(0);
+        dummyhead->next=head;
+        // cur指的是被删的前一个，帮助删当前那个！
+        ListNode* cur=dummyhead;
+        
+        while(cur->next->next){
+            if(cur->next->val==val){
+                ListNode* tmp=cur->next;
+                cur->next=cur->next->next; //cur的next成员内容是cur的next指的那个指针的成员next
+                delete tmp;
+            }
+            else cur=cur->next;
+        }        
     }
 };
-
-int main(){
-    vector<int> nums;
-    int num, target;
-    while(cin >> num){
-        if(getchar() == '\n') break;
-        nums.push_back(num);
-    }
-
-    cin >> target;
-
-    cout << Solution().removeElement(nums,target) << endl;
-    return 0;
-}
